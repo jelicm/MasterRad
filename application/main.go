a783d27ed85b38b17bfef4355a7694cc1000e0b5
jelicm <jelicm2000@gmail.com>
2024-07-20 17:15:15 +0200

    docker compose and application v1

---
+++ application/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/nats-io/nats.go"
+)
+
+func main() {
+	fmt.Println("neka funkcajo")
+
+	conn := Conn()
+	defer conn.Close()
+
+	subject := "app32+app23/Root/file.txt"
+
+	err := conn.Publish(subject, []byte("aaaajo"))
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+func Conn() *nats.Conn {
+	conn, err := nats.Connect("nats://localhost:4222")
+	if err != nil {
+		log.Fatal(err)
+	}
+	return conn
+}
